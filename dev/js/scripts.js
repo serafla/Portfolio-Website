540fc3bd13929502fab690bf55114f7d1be1320f
serafla <63385649+serafla@users.noreply.github.com>
2022-03-09 19:20:06 -0500

    filterizr issues pt 2 / updated database

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -29,11 +29,64 @@ window.addEventListener('load', function (){
     });
    
     //FILTERIZR 
-    
+
     // commented out code from filter.js file to put in scripts.js 
     // these are the initializing settings which you can customize thru options
+
+  //  function filter(){
+  //     console.log("clicks");
     
-    const options = { /* check next step for available options */ };
+  //       filterLinks.forEach((link, i) => {
+  //           link.addEventListener("click", e => {
+  //               e.preventDefault();
+    
+  //               console.log(currentLink);
+  //               // set the currentLink
+
+  //               if(i == 0){
+  //                 const options = { 
+  //                   animationDuration: 0.5, 
+  //                   filter: 'all',
+  //                 }
+  //               }
+                
+  //               currentLink = i;
+              
+  //           });
+  //       });
+  //   }
+    
+    // const options = { 
+    //   animationDuration: 0.5, 
+
+    //   // filter: 'all',
+    //   // filterOutCss: {
+    //   //   opacity: 0,
+    //   //   transform: 'scale(0.5)'
+    //   // },
+
+    //   // filterInCss: {
+    //   // opacity: 0,
+    //   // transform: 'scale(1)' },
+
+    //   gridItemsSelector: '.filtr-item',
+    //   gutterPixels: 0,
+    //   layout: ,
+    //   multifilterLogicalOperator: 'or',
+    //   setupControls: true,
+    //   spinner: {
+    //     enabled: false,
+    //     fillColor: '#2184D0',
+    //     styles: {
+    //       height: '75px',
+    //       margin: '0 auto',
+    //       width: '75px',
+    //       'z-index': 2
+    //     }
+    //   }
+    //  };
+
+     
 
       // Adjust the CSS selector to match the container where
     // you set up your image gallery
