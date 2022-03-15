ae8f815eb80ba539b6782a54fa0e4df386e570ca
serafla <63385649+serafla@users.noreply.github.com>
2022-03-15 19:09:51 -0400

    added lots of things

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,13 +1,14 @@
 import {Carousel} from "@fancyapps/ui";
 import { projects } from "./projects"
-import Filterizr from 'filterizr';
-// import { clickFilter } from "./filter"
+
 
 window.addEventListener('load', function (){
 
-    const myCarousel = new Carousel(document.querySelector(".carousel"), {
+    const heroCarousel = new Carousel(document.querySelector("#hero-carousel"), {
       'center' : false
     });
+
+    
 
     // populate the cover images
     let coverImgArray = document.querySelectorAll('.portfolio-cover-image');
@@ -27,71 +28,18 @@ window.addEventListener('load', function (){
     tagArray.forEach((tag, i) => {
       tag.innerHTML = projects[i].tag;
     });
+
+    let heroLinks = document.querySelectorAll("#gallery a");
+
+    heroLinks.forEach((link, i) => {
+      // console.log("hello");
+
+      link.addEventListener("click", () =>{
+          localStorage.setItem("indexValue", i);
+          link.href = "project_template.html?=" + projects[i].title;
+      });
+  });
    
-    //FILTERIZR 
-
-    // commented out code from filter.js file to put in scripts.js 
-    // these are the initializing settings which you can customize thru options
-
-  //  function filter(){
-  //     console.log("clicks");
-    
-  //       filterLinks.forEach((link, i) => {
-  //           link.addEventListener("click", e => {
-  //               e.preventDefault();
-    
-  //               console.log(currentLink);
-  //               // set the currentLink
-
-  //               if(i == 0){
-  //                 const options = { 
-  //                   animationDuration: 0.5, 
-  //                   filter: 'all',
-  //                 }
-  //               }
-                
-  //               currentLink = i;
-              
-  //           });
-  //       });
-  //   }
-    
-    // const options = { 
-    //   animationDuration: 0.5, 
-
-    //   // filter: 'all',
-    //   // filterOutCss: {
-    //   //   opacity: 0,
-    //   //   transform: 'scale(0.5)'
-    //   // },
-
-    //   // filterInCss: {
-    //   // opacity: 0,
-    //   // transform: 'scale(1)' },
-
-    //   gridItemsSelector: '.filtr-item',
-    //   gutterPixels: 0,
-    //   layout: ,
-    //   multifilterLogicalOperator: 'or',
-    //   setupControls: true,
-    //   spinner: {
-    //     enabled: false,
-    //     fillColor: '#2184D0',
-    //     styles: {
-    //       height: '75px',
-    //       margin: '0 auto',
-    //       width: '75px',
-    //       'z-index': 2
-    //     }
-    //   }
-    //  };
-
-     
-
-      // Adjust the CSS selector to match the container where
-    // you set up your image gallery
-    const filterizr = new Filterizr('#filter-container', options);
-
 });
 
 
---
+++ dev/js/template.js
@@ -0,0 +1,97 @@
+import { projects } from "./projects"
+// import {Carousel} from "@fancyapps/ui";
+import { gsap } from "gsap";
+
+window.addEventListener('load', function() {
+
+  // const templateCarousel = new Carousel(document.querySelector("#project-carousel"), {
+  //     'center' : false
+  //   });
+
+    // console.log(projects[localStorage.getItem("indexValue")].images[1]);
+
+    //populate title
+    let title = document.querySelector("#template-hero-text h1");
+    title.innerHTML = projects[localStorage.getItem("indexValue")].title;
+
+     //populate cover img
+     let coverImg = document.querySelector("#template-hero");
+     coverImg.style.backgroundImage = "url(" + projects[localStorage.getItem("indexValue")].cover_image[0] + ")";
+
+     //populate tag
+     let tag = document.querySelector("#template-hero-text h3");
+     tag.innerHTML = projects[localStorage.getItem("indexValue")].tag;
+
+      // populate components
+      let components = document.querySelector("#components-container li");
+      components.innerHTML = projects[localStorage.getItem("indexValue")].components;
+
+       //populate description
+     let description = document.querySelector("#description-container p");
+     description.innerHTML = projects[localStorage.getItem("indexValue")].description;
+
+      //populate process description
+      let processDescription = document.querySelector("#process-type p");
+      processDescription.innerHTML = projects[localStorage.getItem("indexValue")].process_description;
+
+       //populate process imgs
+       let imgProcessArray = this.document.querySelectorAll(".process-images");
+       imgProcessArray.forEach((image, i) => {
+        imgProcessArray[i].style.backgroundImage = "url(" + projects[localStorage.getItem("indexValue")].process_imgs[i] + ")";
+    });
+
+      //populate brand guide
+      let brandGuide = document.querySelector("#brand-guide-image");
+      brandGuide.style.backgroundImage = "url(" + projects[localStorage.getItem("indexValue")].brand_guide[0] + ")";
+
+      // populate video
+    let video = document.querySelector("#myVideo");
+    // check if there is data under video tag in projects.js
+      if(projects[localStorage.getItem("indexValue")].video === ""){
+        gsap.set("#myVideo", {display: "none"});
+      }else{
+        video.src = projects[localStorage.getItem("indexValue")].video;
+      }
+
+      
+
+      // // populate carousel OR video
+      // let carouselImages = document.querySelector("#project-carousel");
+      // let video = document.querySelector("#myVideo");
+      // if (projects[localStorage.getItem("indexValue")].carousel === "0"){
+      //   carouselImages.forEach((carousel, i) => {
+      //     carouselImages[i].style.backgroundImage = "url(" + projects[localStorage.getItem("indexValue")].carousel[i] + ")";
+      //     gsap.set("#myVideo", {display: "none"});
+      // });
+      // } else if (projects[localStorage.getItem("indexValue")].video === "0"){
+      //   video.innerHTML += 'source src="' + projects[localStorage.getItem("indexValue")].video[0] +'">';
+      //   gsap.set("#project-carousel", {display: "none"});
+      // } else {
+      //   gsap.set("#myVideo", {display: "none"});
+      //   gsap.set("#project-carousel", {display: "none"});
+      // }
+
+      // video.innerHTML += '<source src="'+ source +'">';
+
+      // <source src="img/SEPTA/SEPTA-GIF.mp4" type="video/mp4"></source>
+      // "url(" + projects[localStorage.getItem("indexValue")].images[1] + ")";
+
+
+    // populate images
+    let imgDetailsArray = this.document.querySelectorAll(".project-image");
+    // check if there is data under images tag in projects.js
+    if(projects[localStorage.getItem("indexValue")].images === ""){
+      gsap.set("#project-gallery ul", {display: "none"});
+    }else{
+      imgDetailsArray.forEach((image, i) => {
+        imgDetailsArray[i].style.backgroundImage = "url(" + projects[localStorage.getItem("indexValue")].images[i] + ")";
+    });
+    }
+
+    // let heroImg = document.querySelector("#detail-hero");
+    // heroImg.style.backgroundImage = "url(" + projects[localStorage.getItem("indexValue")].images[1] + ")";
+
+    // let moreImg = document.querySelector("#moreImg");
+    // moreImg.style.backgroundImage = "url(" + projects[localStorage.getItem("indexValue")].images[0] + ")";
+    
+});
